092237a1eb8e4d6dc4575890fabf3df1dee48cb1
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-10-07 16:59:59 +0300

    correct startGame() and main()

--- src/main/java/forHxH/game/App.java
+++ src/main/java/forHxH/game/App.java
@@ -10,25 +10,32 @@ public class App {
     public static final String TURNS_ORDER_QUESTION = "Are you attacking (A) a monster or will you defend" +
             " (D) yourself? WARNING! The monster can win with one hit. I recommend attacking first!" +
             " Enter your choice - A/D.";
+    public static final String BYE_MESSAGE = "Goodbye!";
     public static final String WRONG_START_INPUT_ERROR = "Wrong input. Please, input 'A' or 'D' to start game" +
             " or 'Q' to exit.";
 
     public static void main(String[] args) {
         System.out.println(INTRODUCTION_MESSAGE);
         System.out.println(TURNS_ORDER_QUESTION);
-
-        try(Scanner gameStartScanner = new Scanner(System.in)) {
-            String playerTurnOrderChoice = gameStartScanner.next();
-            startGame(playerTurnOrderChoice);
-        }
+        startGame();
     }
 
-    private static void startGame(String playerTurnOrderChoice) {
-        switch (playerTurnOrderChoice) {
-            case "A" -> GamePreparation.prepareGame(CREATURE_TYPE_GAMER);
-            case "D" -> GamePreparation.prepareGame(CREATURE_TYPE_MONSTER);
-            case "Q" -> System.exit(0);
-            default -> System.out.println(WRONG_START_INPUT_ERROR);
+    private static void startGame() {
+        try(Scanner gameStartScanner = new Scanner(System.in)) {
+            String playerTurnOrderChoice = gameStartScanner.next();
+
+            switch (playerTurnOrderChoice) {
+                case "A" -> GamePreparation.prepareGame(CREATURE_TYPE_GAMER);
+                case "D" -> GamePreparation.prepareGame(CREATURE_TYPE_MONSTER);
+                case "Q" -> {
+                    System.out.println(BYE_MESSAGE);
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println(WRONG_START_INPUT_ERROR);
+                    startGame();
+                }
+            }
         }
     }
 }
