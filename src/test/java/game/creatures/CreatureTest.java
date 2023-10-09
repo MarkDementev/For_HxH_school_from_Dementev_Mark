71661c94c14b38025e359205faf7b700dc57fcad
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-10-09 17:22:43 +0300

    correct GamerTest

--- src/test/java/game/creatures/CreatureTest.java
+++ src/test/java/game/creatures/CreatureTest.java
@@ -1,31 +1,77 @@
 package game.creatures;
 
-import static game.creatures.GamerTest.TEST_GAMER_ATTACK_VALUE;
-import static game.creatures.GamerTest.TEST_GAMER_DAMAGE_RANGE_VALUE;
-import static game.creatures.GamerTest.TEST_GAMER_PROTECTION_VALUE;
-import static game.creatures.GamerTest.TEST_GAMER_HEALTH_VALUE;
+import static game.utils.Generator.VARIABLE_ERROR;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import game.utils.Generator;
 import org.junit.jupiter.api.Test;
 
 public class CreatureTest {
-    public static final int TEST_MONSTER_ATTACK_VALUE = Generator.ATTACK_MAX_VALUE;
-    public static final int[] TEST_MONSTER_DAMAGE_RANGE_VALUE = {Generator.DAMAGE_MIN_VALUE,
-            Generator.DAMAGE_MAX_VALUE};
-    public static final int TEST_MONSTER_PROTECTION_VALUE = Generator.PROTECTION_MAX_VALUE;
-    public static final int TEST_MONSTER_HEALTH_VALUE = Generator.HEALTH_MAX_VALUE;
-    public static final int CREATURE_TESTS_STEPS = 100;
-
-    @Test
-    public void attackAndCalculateDamage() {
-        Gamer gamer = new Gamer(TEST_GAMER_ATTACK_VALUE, TEST_GAMER_DAMAGE_RANGE_VALUE,
-                TEST_GAMER_PROTECTION_VALUE, TEST_GAMER_HEALTH_VALUE);
-        Monster monster = new Monster(TEST_MONSTER_ATTACK_VALUE, TEST_MONSTER_DAMAGE_RANGE_VALUE,
-                TEST_MONSTER_PROTECTION_VALUE, TEST_MONSTER_HEALTH_VALUE);
-        gamer.attackAndCalculateDamage(monster);
-    }
-
-    @Test
-    public void attackAndCalculateDamageWithException() {
-
-    }
+//    @Test
+//    public void attackAndCalculateDamage() {
+//        Gamer gamer = new Gamer(Generator.ATTACK_MAX_VALUE,
+//                new int[]{Generator.DAMAGE_MIN_VALUE, Generator.DAMAGE_MAX_VALUE},
+//                Generator.PROTECTION_MAX_VALUE,
+//                Generator.HEALTH_MAX_VALUE);
+//        Monster monster = new Monster(Generator.ATTACK_MAX_VALUE,
+//                new int[]{Generator.DAMAGE_MIN_VALUE, Generator.DAMAGE_MAX_VALUE},
+//                Generator.PROTECTION_MAX_VALUE,
+//                Generator.HEALTH_MAX_VALUE);
+//
+//        IllegalArgumentException thrownFirst = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownFirst.getMessage().contains(VARIABLE_ERROR + "currentHealth"));
+//
+//        gamer.setCurrentHealth(TEST_GAMER_HEALTH_VALUE_VERY_BIG);
+//        IllegalArgumentException thrownSecond = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownSecond.getMessage().contains(VARIABLE_ERROR + "currentHealth"));
+//
+//        gamer.setCurrentHealth(TEST_GAMER_HEALTH_VALUE);
+//        gamer.setHealCount(TEST_GAMER_HEAL_COUNT_VALUE_VERY_SMALL);
+//        IllegalArgumentException thrownThird = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownThird.getMessage().contains(VARIABLE_ERROR + "attack"));
+//
+//        gamer.setHealCount(TEST_GAMER_HEAL_COUNT_VALUE_VERY_BIG);
+//        IllegalArgumentException thrownFourth = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownFourth.getMessage().contains(VARIABLE_ERROR + "attack"));
+//
+//        IllegalArgumentException thrownFirst = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownFirst.getMessage().contains(VARIABLE_ERROR + "protection"));
+//
+//        gamer.setCurrentHealth(TEST_GAMER_HEALTH_VALUE_VERY_BIG);
+//        IllegalArgumentException thrownSixth = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownSecond.getMessage().contains(VARIABLE_ERROR + "protection"));
+//
+//        gamer.setCurrentHealth(TEST_GAMER_HEALTH_VALUE);
+//        gamer.setHealCount(TEST_GAMER_HEAL_COUNT_VALUE_VERY_SMALL);
+//        IllegalArgumentException thrownSeventh = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownThird.getMessage().contains(VARIABLE_ERROR + "damageRange"));
+//
+//        gamer.setHealCount(TEST_GAMER_HEAL_COUNT_VALUE_VERY_BIG);
+//        IllegalArgumentException thrownEighth = assertThrows(
+//                IllegalArgumentException.class,
+//                gamer::healYourself
+//        );
+//        assertTrue(thrownFourth.getMessage().contains(VARIABLE_ERROR + "damageRange"));
+//    }
 }
